15d7c83c4f6ac0e04bf5285c933626509a04f0fd
prd-hai-truong <hai.truong@paradox.ai>
2022-06-13 17:45:34 +0700

    Update backend

---
+++ django_backend/product/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class LatestProductsList(APIView):
+
